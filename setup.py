4aa9c139fe30f1014c1c43d3e34c21b0b84e5c44
CPapadim <papadimitriou.c@gmail.com>
2018-08-01 13:04:09 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='data_connectors',
+      version='0.1',
+      description='Helper functions to connect to various data sources',
+      url='https://github.com/CPapadim/data_connectors',
+      author='Charalampos Papadimitriou',
+      author_email='papadimitriou.c@gmail.com',
+      license='MIT',
+      packages=['data_connectors'],
+      zip_safe=False)
